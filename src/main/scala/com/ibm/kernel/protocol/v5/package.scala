1e166f9058e33a3337b7b5c7a59c92fec66c0ece
Chip Senkbeil <rcsenkbe@us.ibm.com>
2014-07-24 12:18:48 -0500

    Added UUID type reference to String to v5 protocol

--- src/main/scala/com/ibm/kernel/protocol/v5/package.scala
+++ src/main/scala/com/ibm/kernel/protocol/v5/package.scala
@@ -1,6 +1,9 @@
 package com.ibm.kernel.protocol
 
 package object v5 {
+  // Provide a UUID type representing a string (there is no object)
+  type UUID = String
+
   // Provide a ParentHeader type and object representing a Header
   type ParentHeader = Header
   val ParentHeader = Header
