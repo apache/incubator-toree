79d559f553347c01369296bb1cf6f03d3bf3da22
Chip Senkbeil <pair+rcsenkbe@us.ibm.com>
2014-07-24 14:42:07 -0500

    Added InspectRequest content type

---
+++ src/main/scala/com/ibm/kernel/protocol/v5/content/InspectRequest.scala
@@ -0,0 +1,16 @@
+package com.ibm.kernel.protocol.v5.content
+
+import play.api.libs.json._
+
+case class InspectRequest(
+  code: String,
+  cursor_pos: Int,
+  detail_level: Int // TODO: This is currently either 0 or 1... should we
+                    // TODO: look into enforcing that in our schema?
+)
+
+object InspectRequest {
+  implicit val inspectRequestReads = Json.reads[InspectRequest]
+  implicit val inspectRequestWrites = Json.writes[InspectRequest]
+}
+
