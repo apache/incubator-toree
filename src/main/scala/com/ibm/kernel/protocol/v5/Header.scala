65459237e0a3a61e7b67c8265e51cc9f19db696d
Chip Senkbeil <rcsenkbe@us.ibm.com>
2014-07-24 12:19:19 -0500

    Refactored Json Play definitions in Header to use the provided Json Play macros for read/write

--- src/main/scala/com/ibm/kernel/protocol/v5/Header.scala
+++ src/main/scala/com/ibm/kernel/protocol/v5/Header.scala
@@ -1,17 +1,21 @@
 package com.ibm.kernel.protocol.v5
 
 import play.api.libs.json._
-import play.api.libs.json.Reads._
-import play.api.libs.functional.syntax._
 
 case class Header(
-  msgId: String,    // IScala has a custom UUID type for this (protocol says UUID too)
+  msg_id: UUID,    // IScala has a custom UUID type for this (protocol says UUID too)
   username: String,
-  session: String,  // Custom UUID type also used here
-  msgType: String,
+  session: UUID,  // Custom UUID type also used here
+  msg_type: String,
   version: String   // Assuming a string since the doc says '5.0'
 )
 
+object Header {
+  implicit val headerReads = Json.reads[Header]
+  implicit val headerWrites = Json.writes[Header]
+}
+
+/* LEFT FOR REFERENCE IN CREATING CUSTOM READ/WRITE
 object Header {
   implicit val headerReads: Reads[Header] = (
     (JsPath \ "msg_id").read[String] and
@@ -29,3 +33,4 @@ object Header {
     (JsPath \ "version").write[String]
   )(unlift(Header.unapply)) // Case class provides the unapply method
 }
+*/
