50369b8acfdac6a36b4b687a104c655c7639894b
Chip Senkbeil <pair+rcsenkbe@us.ibm.com>
2014-07-29 11:52:35 -0500

    Added initial test isolation via Build.scala (mixes with build.sbt)

---
+++ project/Build.scala
@@ -0,0 +1,26 @@
+import sbt._
+import Keys._
+
+object B extends Build {
+  lazy val root =
+    Project("root", file("."))
+      .configs( UnitTest )
+      .configs( IntegrationTest )
+      .configs( SystemTest )
+      .settings( inConfig(UnitTest)(Defaults.testTasks) : _*)
+      .settings( inConfig(IntegrationTest)(Defaults.testTasks) : _*)
+      .settings( inConfig(SystemTest)(Defaults.testTasks) : _*)
+      .settings(
+        testOptions in UnitTest := Seq(Tests.Filter(unitFilter)),
+        testOptions in IntegrationTest := Seq(Tests.Filter(intFilter)),
+        testOptions in SystemTest := Seq(Tests.Filter(sysFilter))
+      )
+
+  def sysFilter(name: String): Boolean = name endsWith "SpecForSystem"
+  def intFilter(name: String): Boolean = name endsWith "SpecForIntegration"
+  def unitFilter(name: String): Boolean = (name endsWith "Spec") && !intFilter(name) && !sysFilter(name)
+
+  lazy val UnitTest = config("unit") extend(Test)
+  lazy val IntegrationTest = config("integration") extend(Test)
+  lazy val SystemTest = config("system") extend(Test)
+}
