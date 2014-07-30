dcdf3bcadf8c9df297a603af7d49d2e59babb406
Corey Stubbs <pair+cstubbs@us.ibm.com>
2014-07-29 19:55:11 -0500

    Added SocketConfig class for implicit parsing of ZeroMQ socket configuration.

---
+++ src/main/scala/com/ibm/kernel/protocol/v5/SocketConfig.scala
@@ -0,0 +1,20 @@
+package com.ibm.kernel.protocol.v5
+
+import play.api.libs.json.Json
+
+case class SocketConfig (
+  stdin_port: Int,
+  control_port: Int,
+  hb_port: Int,
+  shell_port: Int,
+  iopub_port: Int,
+  ip : String,
+  transport: String,
+  signature_scheme: String,
+  key: String
+)
+
+object SocketConfig {
+  implicit val socketConfigReads = Json.reads[SocketConfig]
+  implicit val socketConfigWrites = Json.writes[SocketConfig]
+}
