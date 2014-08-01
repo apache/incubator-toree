fa4fe3a154a46706597ea34452736fa28a7e8110
Chip Senkbeil <rcsenkbe@us.ibm.com>
2014-08-01 05:20:09 -0500

    Added DynamicSupport class, allowing reflective magic with 'missing_method' capabilities similar to Ruby - to be used with wrapper classes in interpreter

---
+++ src/main/scala/com/ibm/spark/utils/DynamicSupport.scala
@@ -0,0 +1,110 @@
+package com.ibm.spark.utils
+
+import language.dynamics
+import language.existentials
+
+import java.lang.reflect.Method
+
+/**
+ * Represents dynamic support capabilities for a class. Provides field and
+ * method catchers to reflectively execute potentially-missing cases.
+ * @param klass The class whose fields and methods to access
+ * @param instance The specific instance whose fields and methods to access
+ */
+case class DynamicSupport(
+  private val klass: Class[_], private val instance: Any
+) extends Dynamic {
+
+  /**
+   * Handles cases of field access not found from type-checks. Attempts to
+   * reflectively access field from provided class (or instance). Will first
+   * check for a method signature matching field name due to Scala
+   * transformation process.
+   * @param name The name of the field
+   * @return The content held by the field
+   */
+  def selectDynamic(name: String): Any = {
+    val method = getMethod(name, Nil)
+    method match {
+      case Some(m) => invokeMethod(m, Nil)
+      case _ =>
+        val field = klass.getDeclaredField(name)
+        field.setAccessible(true)
+        field.get(instance)
+    }
+  }
+
+  /**
+   * Handles cases of method not found from type-checks. Attempts to
+   * reflectively execute the method from the provided class (or instance).
+   * Will search for a method signature that has matching parameters with
+   * arguments allowed to be subclasses of parameter types.
+   *
+   * @note Chaining in not supported. You must first cast the result of the
+   *       execution before attempting to apply a method upon it.
+   *
+   * @param name The name of the method
+   * @param args The list of arguments for the method
+   * @return The result of the method's execution
+   */
+  def applyDynamic(name: String)(args: Any*) = {
+    val method = getMethod(name, args.toList)
+
+    method match {
+      case Some(m) => invokeMethod(m, args.toList)
+      case _ =>
+        throw new NoSuchMethodException(
+          klass.getName + "." + name +
+            "(" + args.map(_.getClass.getName).mkString(",") + ")"
+        )
+    }
+  }
+
+  private def getMethod(name: String, args: Any*): Option[Method] = {
+    val flatArgs = flatten(args)
+    val potentialMethods = klass.getDeclaredMethods.filter(_.getName == name)
+    val method: Option[Method] =
+      potentialMethods.foldLeft[Option[Method]](None) {
+        (current, m) =>
+          if (current != None) current
+          else if (m.getParameterTypes.size != flatArgs.size) current
+          else if (!m.getParameterTypes.zipWithIndex.forall {
+            case (c, i) => isCompatible(c, flatArgs(i).getClass)
+          }) current
+          else Some(m)
+      }
+
+    method
+  }
+
+  private def invokeMethod(method: Method, args: Any*) = {
+    val flatArgs = flatten(args).map(_.asInstanceOf[AnyRef])
+    method.invoke(instance, flatArgs: _*)
+  }
+
+  private def flatten(l: Seq[Any]): Seq[Any] = {
+    l.flatMap {
+      case newList: List[_] => flatten(newList)
+      case x => List(x)
+    }
+  }
+
+  private def isCompatible(klazz1: Class[_], klazz2: Class[_]): Boolean = {
+    var result =
+      klazz1.isAssignableFrom(klazz2) ||
+      klazz1.isInstance(klazz2) ||
+      klazz1.isInstanceOf[klazz2.type]
+
+    if (!result) {
+      try {
+        klazz1.asInstanceOf[klazz2.type]
+        result = true
+      } catch {
+        case _: Throwable => result = false
+      }
+    }
+
+    result
+  }
+
+}
---
+++ src/test/scala/com/ibm/spark/utils/DynamicSupportSpec.scala
@@ -0,0 +1,165 @@
+package com.ibm.spark.utils
+
+import java.io.OutputStream
+
+import org.mockito.Matchers._
+import org.mockito.Mockito._
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{GivenWhenThen, BeforeAndAfter, FunSpec, Matchers}
+
+class DynamicSupportSpec
+  extends FunSpec with Matchers with MockitoSugar {
+
+  describe("DynamicSupport") {
+    describe("with a class instance") {
+      describe("#selectDynamic") {
+        it("should support accessing a normal field") {
+          class MyTestClass {
+            val test = 3
+          }
+
+          val x: MyTestClass = new MyTestClass
+
+          val dynamicSupport = DynamicSupport(x.getClass, x)
+
+          dynamicSupport.test should be (3)
+        }
+
+        it("should support accessing a method with no arguments") {
+          class MyTestClass {
+            def test = 3
+          }
+
+          val x: MyTestClass = new MyTestClass
+
+          val dynamicSupport = DynamicSupport(x.getClass, x)
+
+          dynamicSupport.test should be (3)
+        }
+
+        it("should throw an error if the field does not exist") {
+          class MyTestClass
+
+          val x: MyTestClass = new MyTestClass
+
+          val dynamicSupport = DynamicSupport(x.getClass, x)
+
+          intercept[NoSuchFieldException] {
+            dynamicSupport.test
+          }
+        }
+      }
+
+      describe("#applyDynamic") {
+        it("should support executing a method with one argument") {
+          class MyTestClass {
+            def test(x: Int) = x
+          }
+
+          val x: MyTestClass = new MyTestClass
+
+          val dynamicSupport = DynamicSupport(x.getClass, x)
+
+          dynamicSupport.test(5) should be (5)
+        }
+
+        it("should support executing a method with multiple arguments") {
+          class MyTestClass {
+            def test(x: Int, y: String) = (x, y)
+          }
+
+          val x: MyTestClass = new MyTestClass
+
+          val dynamicSupport = DynamicSupport(x.getClass, x)
+
+          dynamicSupport.test(5, "test me") should be (5, "test me")
+        }
+
+        it("should throw an error if the method does not exist") {
+          class MyTestClass
+
+          val x: MyTestClass = new MyTestClass
+
+          val dynamicSupport = DynamicSupport(x.getClass, x)
+
+          intercept[NoSuchMethodException] {
+            dynamicSupport.test(5, "test me")
+          }
+        }
+      }
+    }
+
+    describe("with an object") {
+      describe("#selectDynamic") {
+        it("should support accessing a normal field") {
+          object MyTestObject {
+            val test = 3
+          }
+
+          val dynamicSupport =
+            DynamicSupport(MyTestObject.getClass, MyTestObject)
+
+          dynamicSupport.test should be (3)
+        }
+
+        it("should support accessing a method with no arguments") {
+          object MyTestObject {
+            def test = 3
+          }
+
+          val dynamicSupport =
+            DynamicSupport(MyTestObject.getClass, MyTestObject)
+
+          dynamicSupport.test should be (3)
+        }
+
+        it("should throw an error if the field does not exist") {
+          object MyTestObject
+
+          val dynamicSupport =
+            DynamicSupport(MyTestObject.getClass, MyTestObject)
+
+          intercept[NoSuchFieldException] {
+            dynamicSupport.test
+          }
+        }
+      }
+
+      describe("#applyDynamic") {
+        it("should support executing a method with one argument") {
+          object MyTestObject {
+            def test(x: Int) = x
+          }
+
+          val dynamicSupport =
+            DynamicSupport(MyTestObject.getClass, MyTestObject)
+
+          dynamicSupport.test(5) should be (5)
+        }
+
+        it("should support executing a method with multiple arguments") {
+          object MyTestObject {
+            def test(x: Int, y: String) = (x, y)
+          }
+
+          val dynamicSupport =
+            DynamicSupport(MyTestObject.getClass, MyTestObject)
+
+          dynamicSupport.test(5, "test me") should be (5, "test me")
+
+        }
+
+        it("should throw an error if the method does not exist") {
+          object MyTestObject
+
+          val dynamicSupport =
+            DynamicSupport(MyTestObject.getClass, MyTestObject)
+
+          intercept[NoSuchMethodException] {
+            dynamicSupport.test(5, "test me")
+          }
+        }
+      }
+    }
+  }
+}
