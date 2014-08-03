813f740b762db407d4aca36f8ddc4650adb37ff2
Chip Senkbeil <rcsenkbe@us.ibm.com>
2014-08-03 14:38:15 -0500

    Added HMAC security class

---
+++ src/main/scala/com/ibm/spark/security/Hmac.scala
@@ -0,0 +1,48 @@
+package com.ibm.spark.security
+
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
+
+object HmacAlgorithm extends Enumeration {
+  type HmacAlgorithm = Value
+
+  def apply(key: String) = Value(key)
+
+  val MD5     = Value("HmacMD5")
+  val SHA1    = Value("HmacSHA1")
+  val SHA256  = Value("HmacSHA256")
+}
+
+import HmacAlgorithm._
+
+object Hmac {
+  
+  def apply(key: String, algorithm: HmacAlgorithm = HmacAlgorithm.SHA256) =
+    new Hmac(key, algorithm)
+  
+  def newMD5(key: String): Hmac     = this(key, HmacAlgorithm.MD5)
+  def newSHA1(key: String): Hmac    = this(key, HmacAlgorithm.SHA1)
+  def newSHA256(key: String): Hmac  = this(key, HmacAlgorithm.SHA256)
+}
+
+class Hmac(
+  val key: String,
+  val algorithm: HmacAlgorithm = HmacAlgorithm.SHA256
+) {
+
+  private var mac: Mac = _
+  private var secretKeySpec: SecretKeySpec = _
+
+  if (key.nonEmpty) {
+    mac = Mac.getInstance(algorithm.toString)
+    secretKeySpec = new SecretKeySpec(key.getBytes, algorithm.toString)
+    mac.init(secretKeySpec)
+  }
+
+  def apply(items: Seq[String]): String = digest(items)
+
+  def digest(items: Seq[String]): String = if (key.nonEmpty) {
+    items.map(_.getBytes).foreach(mac.update)
+    mac.doFinal.map("%02x" format _).mkString
+  } else ""
+}
---
+++ src/test/scala/com/ibm/spark/security/HmacSpec.scala
@@ -0,0 +1,94 @@
+package com.ibm.spark.security
+
+import java.security.NoSuchAlgorithmException
+
+import org.scalatest.{Matchers, FunSpec}
+import org.mockito.Matchers._
+
+class HmacSpec extends FunSpec with Matchers {
+  describe("Hmac Object") {
+    describe("#apply") {
+      it("should fail if the algorithm is not available") {
+        val nonEmptyKey = "FILL"
+        val badAlgorithm = "One day, I want to be a real algorithm"
+
+        intercept[NoSuchAlgorithmException] {
+          val hmac = Hmac(nonEmptyKey, HmacAlgorithm(badAlgorithm))
+        }
+      }
+
+      it("should succeed if the algorithm is available") {
+        val goodAlgorithm = HmacAlgorithm.SHA256
+
+        val hmac = Hmac(anyString(), goodAlgorithm)
+        hmac.algorithm should be (goodAlgorithm)
+      }
+    }
+
+    describe("#newMD5") {
+      it("should produce an Hmac with the algorithm set to MD5") {
+        val hmac = Hmac.newMD5(anyString())
+
+        hmac.algorithm should be(HmacAlgorithm.MD5)
+      }
+    }
+
+    describe("#newSHA1") {
+      it("should produce an Hmac with the algorithm set to SHA1") {
+        val hmac = Hmac.newSHA1(anyString())
+
+        hmac.algorithm should be(HmacAlgorithm.SHA1)
+      }
+    }
+
+    describe("#newSHA256") {
+      it("should produce an Hmac with the algorithm set to SHA256") {
+        val hmac = Hmac.newSHA256(anyString())
+
+        hmac.algorithm should be(HmacAlgorithm.SHA256)
+      }
+    }
+  }
+
+  describe("Hmac Class") {
+    describe("#apply") {
+      // TODO: This should really be mocked since we don't care about the
+      //       results, just the send/receive to the underlying implementation
+      it("should produce the correct digest") {
+        val key = "12345"
+        val message = List("This is a test of SHA256 in action.")
+        val expected =
+          "e60e1494b0650875fa5eb8384e357d731358c3559c1f223d69dc43ffe13570bc"
+        val hmac = new Hmac(key, HmacAlgorithm.SHA256)
+
+        hmac(message) should be(expected)
+      }
+    }
+
+    describe("#digest") {
+      // TODO: This should really be mocked since we don't care about the
+      //       results, just the send/receive to the underlying implementation
+      it("should produce the correct digest") {
+        val key = "12345"
+        val message = List("This is a test of SHA256 in action.")
+        val expected =
+          "e60e1494b0650875fa5eb8384e357d731358c3559c1f223d69dc43ffe13570bc"
+        val hmac = new Hmac(key, HmacAlgorithm.SHA256)
+
+        hmac.digest(message) should be(expected)
+      }
+    }
+  }
+
+  describe("HmacAlgorithm") {
+    describe("#apply") {
+      it("should return a value wrapping the string input") {
+        val resultTypeName = HmacAlgorithm(anyString()).getClass.getName
+
+        // NOTE: Test written this way since unable to check directly against
+        //       the Scala enumeration value
+        resultTypeName should be ("scala.Enumeration$Val")
+      }
+    }
+  }
+}
