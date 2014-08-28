c9631ff9710b057805636a332199ef4f7c1c224e
Chip Senkbeil and Marcy Rattner <pair+rcsenkbe+marattne@us.ibm.com>
2014-08-28 16:07:00 -0500

    Added hasContent to check if text/plain is not empty

--- src/main/scala/com/ibm/spark/kernel/protocol/v5/content/ExecuteResult.scala
+++ src/main/scala/com/ibm/spark/kernel/protocol/v5/content/ExecuteResult.scala
@@ -1,13 +1,20 @@
 package com.ibm.spark.kernel.protocol.v5.content
 
 import play.api.libs.json.Json
-import com.ibm.spark.kernel.protocol.v5._
+import com.ibm.spark.kernel.protocol.v5.{Metadata, Data}
 
 case class ExecuteResult (
   execution_count: Int,
   data: Data,
   metadata: Metadata
-)
+) {
+  def hasContent =
+     data != null &&
+     (data.get("text/plain") match {
+       case Some(value) => value != null && value.nonEmpty
+       case None => false
+     })
+}
 
 object ExecuteResult {
   implicit val executeResultReads = Json.reads[ExecuteResult]
--- src/test/scala/com/ibm/spark/kernel/protocol/v5/content/ExecuteResultSpec.scala
+++ src/test/scala/com/ibm/spark/kernel/protocol/v5/content/ExecuteResultSpec.scala
@@ -4,20 +4,56 @@ import org.scalatest.{FunSpec, Matchers}
 import play.api.data.validation.ValidationError
 import play.api.libs.json._
 
+import com.ibm.spark.kernel.protocol.v5._
+
 class ExecuteResultSpec extends FunSpec with Matchers {
   val executeResultJson: JsValue = Json.parse("""
   {
     "execution_count": 999,
-    "data": {},
+    "data": {"text/plain": "resulty result"},
     "metadata": {}
   }
   """)
 
   val executeResult = ExecuteResult(
-    999, Map(), Map()
+    999, Data("text/plain" -> "resulty result"), Metadata()
   )
 
   describe("ExecuteResult") {
+    describe("#hasContent") {
+      it("should be true when data has a non-empty text/plain field") {
+        executeResult.hasContent should be (true)
+      }
+
+      it("should be false if data is null") {
+        val executeResultNoData = ExecuteResult(
+          999, null, Metadata()
+        )
+        executeResultNoData.hasContent should be (false)
+      }
+
+      it("should be false when data does not have a text/plain field") {
+        val executeResultEmptyData = ExecuteResult(
+          999, Data(), Metadata()
+        )
+        executeResultEmptyData.hasContent should be (false)
+
+      }
+
+      it("should be false if text/plain field maps to an empty string") {
+        val executeResultEmptyString = ExecuteResult(
+          999, Data("text/plain" -> ""), Metadata()
+        )
+        executeResultEmptyString.hasContent should be (false)
+      }
+
+      it("should be false if text/plain maps to null") {
+        val executeResultTextPlainNull = ExecuteResult(
+          999, Data("text/plain" -> null), Metadata()
+        )
+        executeResultTextPlainNull.hasContent should be (false)
+      }
+    }
     describe("implicit conversions") {
       it("should implicitly convert from valid json to a ExecuteResult instance") {
         // This is the least safe way to convert as an error is thrown if it fails
