fde69aba4e176a34c23b2916b176d743bd4b9e26
Chip Senkbeil <pair+rcsenkbe@us.ibm.com>
2014-09-09 21:42:43 -0500

    Added blank describes for ScalaInterpreter

---
+++ src/test/scala/com/ibm/spark/interpreter/ScalaInterpreterSpec.scala
@@ -0,0 +1,43 @@
+package com.ibm.spark.interpreter
+
+import org.scalatest.{Matchers, FunSpec}
+
+class ScalaInterpreterSpec extends FunSpec with Matchers {
+  describe("ScalaInterpreter") {
+    describe("#addJars") {
+
+    }
+
+    describe("#interrupt") {
+
+    }
+
+    describe("#interpret") {
+
+    }
+
+    describe("#start") {
+
+    }
+
+    describe("#stop") {
+
+    }
+
+    describe("#updatePrintStreams") {
+
+    }
+
+    describe("#classServerUri") {
+
+    }
+
+    describe("#doQuietly") {
+
+    }
+
+    describe("#bind") {
+
+    }
+  }
+}
