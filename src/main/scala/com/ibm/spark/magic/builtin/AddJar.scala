97bfbb265c418f8ff30a5868442d124f3619c20d
Chip Senkbeil <pair+rcsenkbe@us.ibm.com>
2014-10-22 11:11:00 -0500

    Added --force option to AddJar magic

--- src/main/scala/com/ibm/spark/magic/builtin/AddJar.scala
+++ src/main/scala/com/ibm/spark/magic/builtin/AddJar.scala
@@ -6,17 +6,23 @@ import java.net.URL
 import com.ibm.spark.kernel.protocol.v5.MIMEType
 import com.ibm.spark.magic.MagicOutput
 import com.ibm.spark.magic.dependencies.{IncludeInterpreter, IncludeOutputStream, IncludeSparkContext}
-import com.ibm.spark.utils.DownloadSupport
+import com.ibm.spark.utils.{ArgumentParsingSupport, DownloadSupport}
+
+import scala.io.Source
 
 class AddJar
   extends MagicTemplate with IncludeInterpreter with IncludeSparkContext
-  with IncludeOutputStream with DownloadSupport
+  with IncludeOutputStream with DownloadSupport with ArgumentParsingSupport
 {
   // TODO: Figure out where to put this AND a better location as /tmp does not
   //       keep the jars around forever.
-  private val JarStorageLocation = "/tmp"
+  private[builtin] val JarStorageLocation = "/tmp"
 
   private val HtmlJarRegex = """.*?\/([^\/]*?)$""".r
+
+  // Option to mark redownloading of jars
+  private val _force =
+    parser.accepts("force", "forces redownload of specified jar")
 
   // Lazy because the outputStream is not provided at construction
   private lazy val printStream = new PrintStream(outputStream)
@@ -24,7 +30,9 @@ class AddJar
   /**
    * Downloads and adds the specified jars to the
    * interpreter/compiler/cluster classpaths.
-   * @param code The lines containing the locations of the jars, separated by newline
+   *
+   * @param code The lines containing the locations of the jars,
+   *             separated by newline
    */
   override def executeCell(code: Seq[String]): MagicOutput = {
     code.foreach(executeLine)
@@ -35,29 +43,49 @@ class AddJar
   /**
    * Downloads and adds the specified jar to the
    * interpreter/compiler/cluster classpaths.
+   *
    * @param code The line containing the location of the jar
    */
   override def executeLine(code: String): MagicOutput = {
-    val jarRemoteLocation = code.trim
-    if (jarRemoteLocation.isEmpty)
-      return MagicOutput(MIMEType.PlainText -> s"%AddJar '$jarRemoteLocation' is invalid!")
+    val nonOptionArgs = parseArgs(code.trim)
 
+    // Check valid arguments
+    if (nonOptionArgs.length != 1) {
+      printHelp(printStream, """%AddJar <jar_url>""")
+      return MagicOutput()
+    }
+
+    // Check if the jar we want to download is valid
+    val jarRemoteLocation = nonOptionArgs(0)
+    if (jarRemoteLocation.isEmpty)
+      return MagicOutput(
+        MIMEType.PlainText ->
+          s"%AddJar '$jarRemoteLocation' is invalid!"
+      )
+
+    // Get the destination of the jar
     val HtmlJarRegex(jarName) = jarRemoteLocation
     val downloadLocation = JarStorageLocation + "/" + jarName
+    val fileDownloadLocation = new File(downloadLocation)
 
-    // Report beginning of download
-    printStream.println(s"Starting download from $jarRemoteLocation")
+    // Check if exists in cache or force applied
+    if (_force || !fileDownloadLocation.exists()) {
+      // Report beginning of download
+      printStream.println(s"Starting download from $jarRemoteLocation")
 
-    val jarUrl = downloadFile(
-      new URL(jarRemoteLocation),
-      new File(downloadLocation).toURI.toURL
-    )
+      downloadFile(
+        new URL(jarRemoteLocation),
+        new File(downloadLocation).toURI.toURL
+      )
 
-    // Report download finished
-    printStream.println(s"Finished download of $jarName")
+      // Report download finished
+      printStream.println(s"Finished download of $jarName")
+    } else {
+      printStream.println(s"Using cached version of $jarName")
+    }
 
-    interpreter.addJars(jarUrl)
-    sparkContext.addJar(jarUrl.getPath)
+    interpreter.addJars(fileDownloadLocation.toURI.toURL)
+    sparkContext.addJar(fileDownloadLocation.getCanonicalPath)
 
     MagicOutput() // No output needed
   }
--- src/test/scala/com/ibm/spark/magic/builtin/AddJarSpec.scala
+++ src/test/scala/com/ibm/spark/magic/builtin/AddJarSpec.scala
@@ -1,7 +1,8 @@
 package com.ibm.spark.magic.builtin
 
-import java.io.OutputStream
+import java.io.{File, OutputStream}
 import java.net.URL
+import java.nio.file.{FileSystems, Files}
 
 import com.ibm.spark.interpreter.Interpreter
 import com.ibm.spark.magic.dependencies.{IncludeOutputStream, IncludeInterpreter, IncludeSparkContext}
@@ -62,6 +63,82 @@ class AddJarSpec extends FunSpec with Matchers with MockitoSugar {
 
         addJarMagic.executeLine("""http://www.example.com/someJar.jar""")
 
+        verify(mockSparkContext).addJar(anyString())
+        verify(mockInterpreter).addJars(any[URL])
+      }
+
+      it("should use a cached jar if the force option is not provided") {
+        val mockSparkContext = mock[SparkContext]
+        val mockInterpreter = mock[Interpreter]
+        val mockOutputStream = mock[OutputStream]
+        var downloadFileCalled = false  // Used to verify that downloadFile
+                                        // was or was not called in this test
+
+        val addJarMagic = new AddJar
+          with IncludeSparkContext
+          with IncludeInterpreter
+          with IncludeOutputStream
+        {
+          override val sparkContext: SparkContext = mockSparkContext
+          override val interpreter: Interpreter = mockInterpreter
+          override val outputStream: OutputStream = mockOutputStream
+          override def downloadFile(fileUrl: URL, destinationUrl: URL): URL = {
+            downloadFileCalled = true
+            new URL("file://someFile") // Cannot mock URL
+          }
+        }
+
+        // Create a temporary file representing our jar to fake the cache
+        val tmpFilePath = Files.createTempFile(
+          FileSystems.getDefault.getPath(addJarMagic.JarStorageLocation),
+          "someJar",
+          ".jar"
+        )
+
+        addJarMagic.executeLine(
+          """http://www.example.com/""" + tmpFilePath.getFileName)
+
+        tmpFilePath.toFile.delete()
+
+        downloadFileCalled should be (false)
+        verify(mockSparkContext).addJar(anyString())
+        verify(mockInterpreter).addJars(any[URL])
+      }
+
+      it("should not use a cached jar if the force option is provided") {
+        val mockSparkContext = mock[SparkContext]
+        val mockInterpreter = mock[Interpreter]
+        val mockOutputStream = mock[OutputStream]
+        var downloadFileCalled = false  // Used to verify that downloadFile
+                                        // was or was not called in this test
+
+        val addJarMagic = new AddJar
+          with IncludeSparkContext
+          with IncludeInterpreter
+          with IncludeOutputStream
+        {
+          override val sparkContext: SparkContext = mockSparkContext
+          override val interpreter: Interpreter = mockInterpreter
+          override val outputStream: OutputStream = mockOutputStream
+          override def downloadFile(fileUrl: URL, destinationUrl: URL): URL = {
+            downloadFileCalled = true
+            new URL("file://someFile") // Cannot mock URL
+          }
+        }
+
+        // Create a temporary file representing our jar to fake the cache
+        val tmpFilePath = Files.createTempFile(
+          FileSystems.getDefault.getPath(addJarMagic.JarStorageLocation),
+          "someJar",
+          ".jar"
+        )
+
+        addJarMagic.executeLine(
+          """--force http://www.example.com/""" + tmpFilePath.getFileName)
+
+        tmpFilePath.toFile.delete()
+
+        downloadFileCalled should be (true)
         verify(mockSparkContext).addJar(anyString())
         verify(mockInterpreter).addJars(any[URL])
       }
