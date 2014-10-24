d055a4f6627a7a6297de36fdc1dafb82853eee60
Chip Senkbeil <pair+rcsenkbe@us.ibm.com>
2014-10-24 10:03:58 -0500

    Removed unused sbt launcher

--- bin/run-container.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-BIN=`dirname "$0"`
-BIN=`cd ${BIN}; pwd`
-
-(cd ${BIN}/../; pwd; sbt docker)
-docker run --net=host -e "IP=192.168.44.44" -e "STDIN_PORT=48000" -e "SHELL_PORT=48001" -e "IOPUB_PORT=48002" -e "CONTROL_PORT=48003" -e "HB_PORT=48004" -i com.ibm/spark-kernel:v0.1.0
