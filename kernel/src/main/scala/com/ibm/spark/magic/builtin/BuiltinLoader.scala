8d7c39ca0efb161290f9e519a4de2ef630a2be1f
Chris Marcocci <pair+cjmarcoc@us.ibm.com>
2014-11-24 14:55:37 -0600

    Tests for BuiltinLoader

--- kernel/src/main/scala/com/ibm/spark/magic/builtin/BuiltinLoader.scala
+++ kernel/src/main/scala/com/ibm/spark/magic/builtin/BuiltinLoader.scala
@@ -2,10 +2,8 @@ package com.ibm.spark.magic.builtin
 
 import com.google.common.reflect.ClassPath
 import com.ibm.spark.magic.InternalClassLoader
-
+import com.google.common.base.Strings._
 import scala.collection.JavaConversions._
-
-
 
 /**
  * Represents a class loader that loads classes from the builtin package.
@@ -13,10 +11,17 @@ import scala.collection.JavaConversions._
 class BuiltinLoader
   extends InternalClassLoader(classOf[BuiltinLoader].getClassLoader) {
 
-  def getBuiltinClasses = {
-    val classPath = ClassPath.from(this.getClass.getClassLoader)
-    classPath.getTopLevelClasses("com.ibm.spark.magic.builtin").filter(
-      _.getSimpleName != this.getClass.getSimpleName
-    )
+  private val pkgName = this.getClass.getPackage.getName
+  
+  def getClasses(pkg: String = pkgName) = {
+    isNullOrEmpty(pkg) match {
+      case true =>
+        List()
+      case false =>
+        val classPath = ClassPath.from(this.getClass.getClassLoader)
+        classPath.getTopLevelClasses(pkg).filter(
+          _.getSimpleName != this.getClass.getSimpleName
+        )
+    }
   }
 }
