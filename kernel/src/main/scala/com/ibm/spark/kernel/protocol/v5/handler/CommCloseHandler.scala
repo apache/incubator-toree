321e82917560e7def9b66f61c28af5733b11314d
Chip Senkbeil <chip.senkbeil@gmail.com>
2014-12-23 01:51:22 -0600

    Added CommCloseHandler to trigger close callbacks

---
+++ kernel/src/main/scala/com/ibm/spark/kernel/protocol/v5/handler/CommCloseHandler.scala
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2014 IBM Corp.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.ibm.spark.kernel.protocol.v5.handler
+
+import com.ibm.spark.kernel.protocol.v5.comm.{CommRegistrar, CommWriter, CommStorage}
+import com.ibm.spark.kernel.protocol.v5.content.CommClose
+import com.ibm.spark.kernel.protocol.v5.{KMBuilder, Utilities, ActorLoader, KernelMessage}
+import com.ibm.spark.utils.MessageLogSupport
+import play.api.data.validation.ValidationError
+import play.api.libs.json.JsPath
+
+import scala.concurrent.Future
+import scala.concurrent.future
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class CommCloseHandler(
+  actorLoader: ActorLoader, commRegistrar: CommRegistrar,
+  commStorage: CommStorage
+) extends BaseHandler(actorLoader) with MessageLogSupport
+{
+  override def process(kernelMessage: KernelMessage): Future[_] = {
+    logKernelMessageAction("Initiating Comm Close for", kernelMessage)
+    Utilities.parseAndHandle(
+      kernelMessage.contentString,
+      CommClose.commCloseReads,
+      handler = handleCommClose,
+      errHandler = handleParseError
+    )
+  }
+
+  private def handleCommClose(commClose: CommClose) = future {
+    val commId = commClose.comm_id
+    val data = commClose.data
+
+    logger.debug(s"Received comm_close with id '$commId'")
+
+    // TODO: Should we be reusing something from the KernelMessage?
+    val commWriter = new CommWriter(actorLoader, KMBuilder(), commId)
+
+    if (commStorage.contains(commId)) {
+      logger.debug(s"Executing close callbacks for id '$commId'")
+
+      // TODO: Should we be checking the return values? Probably not.
+      commStorage(commId).executeCloseCallbacks(commWriter, commId, data)
+
+      // Unlink the id with the target
+      commRegistrar.remove(commId)
+    } else {
+      logger.warn(s"Received invalid id for Comm Close: $commId")
+    }
+  }
+
+  private def handleParseError(invalid: Seq[(JsPath, Seq[ValidationError])]) =
+    future {
+      // TODO: Determine proper response for a parse failure
+      logger.warn("Parse error for Comm Close! Not responding!")
+    }
+
+}
+
---
+++ kernel/src/test/scala/com/ibm/spark/kernel/protocol/v5/handler/CommCloseHandlerSpec.scala
@@ -0,0 +1,144 @@
+/*
+ * Copyright 2014 IBM Corp.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.ibm.spark.kernel.protocol.v5.handler
+
+import java.util.UUID
+
+import akka.actor.{Props, ActorRef, ActorSystem}
+import akka.testkit.{TestProbe, ImplicitSender, TestKit}
+import com.ibm.spark.kernel.protocol.v5
+import com.ibm.spark.kernel.protocol.v5.content.{ClearOutput, CommClose}
+import com.ibm.spark.kernel.protocol.v5.{SystemActorType, ActorLoader, KMBuilder}
+import com.ibm.spark.kernel.protocol.v5.comm.{CommRegistrar, CommWriter, CommCallbacks, CommStorage}
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{BeforeAndAfter, FunSpecLike, Matchers}
+import org.mockito.Mockito._
+import org.mockito.Matchers._
+
+import scala.concurrent.duration._
+
+class CommCloseHandlerSpec extends TestKit(
+  ActorSystem("CommCloseHandlerSpec")
+) with ImplicitSender with FunSpecLike with Matchers with MockitoSugar
+  with BeforeAndAfter
+{
+  private val TestCommId = UUID.randomUUID().toString
+  private val TestTargetName = "some test target"
+
+  private var kmBuilder: KMBuilder = _
+  private var mockCommStorage: CommStorage = _
+  private var mockCommCallbacks: CommCallbacks = _
+  private var mockCommRegistrar: CommRegistrar = _
+  private var mockActorLoader: ActorLoader = _
+  private var commCloseHandler: ActorRef = _
+  private var kernelMessageRelayProbe: TestProbe = _
+  private var statusDispatchProbe: TestProbe = _
+
+  before {
+    kmBuilder = KMBuilder()
+    mockCommCallbacks = mock[CommCallbacks]
+    mockCommStorage = mock[CommStorage]
+    doReturn(mockCommCallbacks).when(mockCommStorage)(TestCommId)
+    mockCommRegistrar = mock[CommRegistrar]
+
+    mockActorLoader = mock[ActorLoader]
+
+    commCloseHandler = system.actorOf(Props(
+      classOf[CommCloseHandler],
+      mockActorLoader, mockCommRegistrar, mockCommStorage
+    ))
+
+    // Used to intercept responses
+    kernelMessageRelayProbe = TestProbe()
+    when(mockActorLoader.load(SystemActorType.KernelMessageRelay))
+      .thenReturn(system.actorSelection(kernelMessageRelayProbe.ref.path.toString))
+
+    // Used to intercept busy/idle messages
+    statusDispatchProbe = new TestProbe(system)
+    when(mockActorLoader.load(SystemActorType.StatusDispatch))
+      .thenReturn(system.actorSelection(statusDispatchProbe.ref.path.toString))
+  }
+
+  describe("CommCloseHandler") {
+    describe("#process") {
+      it("should execute close callbacks if the id is registered") {
+        // Mark our id as registered
+        doReturn(true).when(mockCommStorage).contains(TestCommId)
+
+        // Send a comm_open message with the test target
+        commCloseHandler ! kmBuilder
+          .withHeader(v5.MessageType.CommClose)
+          .withContentString(CommClose(TestCommId, v5.Data()))
+          .build
+
+        // Should receive a busy and an idle message
+        statusDispatchProbe.receiveN(2, 200.milliseconds)
+
+        // Verify that the msg callbacks were triggered along the way
+        verify(mockCommCallbacks).executeCloseCallbacks(
+          any[CommWriter], any[v5.UUID], any[v5.Data])
+      }
+
+      it("should unlink the target and Comm id if the id exists") {
+        // Mark our target as registered
+        doReturn(true).when(mockCommStorage).contains(TestCommId)
+
+        // Send a comm_open message with the test target
+        commCloseHandler ! kmBuilder
+          .withHeader(v5.MessageType.CommClose)
+          .withContentString(CommClose(TestCommId, v5.Data()))
+          .build
+
+        // Should receive a busy and an idle message
+        statusDispatchProbe.receiveN(2, 200.milliseconds)
+
+        // Verify that the target and Comm id were unlinked
+        verify(mockCommRegistrar).remove(TestCommId)
+      }
+
+      it("should not execute close callbacks if the id is not registered") {
+        // Mark our target as not registered
+        doReturn(false).when(mockCommStorage).contains(TestCommId)
+
+        // Send a comm_msg message with the test id
+        commCloseHandler ! kmBuilder
+          .withHeader(v5.MessageType.CommClose)
+          .withContentString(CommClose(TestCommId, v5.Data()))
+          .build
+
+        // Should receive a busy and an idle message
+        statusDispatchProbe.receiveN(2, 200.milliseconds)
+
+        // Verify that the msg callbacks were NOT triggered along the way
+        verify(mockCommCallbacks, never()).executeCloseCallbacks(
+          any[CommWriter], any[v5.UUID], any[v5.Data])
+      }
+
+      it("should do nothing if there is a parsing error") {
+        // Send a comm_open message with an invalid content string
+        commCloseHandler ! kmBuilder
+          .withHeader(v5.MessageType.CommClose)
+          .withContentString(ClearOutput(_wait = true))
+          .build
+
+        // TODO: Is there a better way to test for this without an upper time
+        //       limit? Is there a different logical approach?
+        kernelMessageRelayProbe.expectNoMsg(200.milliseconds)
+      }
+    }
+  }
+}
