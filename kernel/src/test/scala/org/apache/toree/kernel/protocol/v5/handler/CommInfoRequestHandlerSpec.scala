fa4d5cd4283deb9d4bd134e4518e15d0c294bcb4
Michael Poplavski <mikepop77@gmail.com>
2016-04-21 11:14:49 -0500

    Added unit tests for Comm Info messages

---
+++ kernel/src/test/scala/org/apache/toree/kernel/protocol/v5/handler/CommInfoRequestHandlerSpec.scala
@@ -0,0 +1,116 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+
+package org.apache.toree.kernel.protocol.v5.handler
+
+import akka.actor.{ActorSelection, ActorSystem, Props}
+import akka.testkit.{ImplicitSender, TestKit, TestProbe}
+import com.typesafe.config.ConfigFactory
+import org.apache.toree.comm.CommStorage
+import org.apache.toree.kernel.protocol.v5.content.CommInfoReply
+import org.apache.toree.kernel.protocol.v5.kernel.ActorLoader
+import org.apache.toree.kernel.protocol.v5.{Header, KernelMessage, SystemActorType}
+import org.mockito.AdditionalMatchers.{not => mockNot}
+import org.mockito.Matchers.{eq => mockEq}
+import org.mockito.Mockito._
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{FunSpecLike, Matchers}
+import play.api.libs.json.Json
+
+import scala.concurrent.duration._
+
+object CommInfoRequestHandlerSpec {
+  val config = """
+    akka {
+      loglevel = "WARNING"
+    }"""
+}
+
+class CommInfoRequestHandlerSpec extends TestKit(
+  ActorSystem("CommInfoRequestHandlerSpec",
+    ConfigFactory.parseString(CommInfoRequestHandlerSpec.config))
+) with ImplicitSender with FunSpecLike with Matchers with MockitoSugar {
+
+  var mockCommStorage: CommStorage = mock[CommStorage]
+
+  val actorLoader: ActorLoader =  mock[ActorLoader]
+  val actor = system.actorOf(Props(classOf[CommInfoRequestHandler], actorLoader, mockCommStorage))
+
+  val relayProbe : TestProbe = TestProbe()
+  val relaySelection : ActorSelection =
+    system.actorSelection(relayProbe.ref.path)
+  when(actorLoader.load(SystemActorType.KernelMessageRelay))
+    .thenReturn(relaySelection)
+  when(actorLoader.load(mockNot(mockEq(SystemActorType.KernelMessageRelay))))
+    .thenReturn(system.actorSelection(""))
+
+  val header = Header("","","","","")
+
+  describe("Comm Info Request Handler") {
+    it("should return a KernelMessage containing comm info response for a specific target name") {
+      val kernelMessage = new KernelMessage(
+        Seq[String](), "test message", header, header, Map[String, String](), "{\"target_name\":\"test.name\"}"
+      )
+
+      when(mockCommStorage.getTargets()).thenReturn(Set("test.name"))
+      when(mockCommStorage.getCommIdsFromTarget("test.name")).thenReturn(Option(scala.collection.immutable.IndexedSeq("1", "2")))
+
+      actor ! kernelMessage
+      val reply = relayProbe.receiveOne(1.seconds).asInstanceOf[KernelMessage]
+      val commInfo = Json.parse(reply.contentString).as[CommInfoReply]
+
+      commInfo.comms.size should equal (2)
+      commInfo.comms.get("1").get.get("target_name").get should be ("test.name")
+      commInfo.comms.get("2").get.get("target_name").get should be ("test.name")
+    }
+  }
+
+  it("should return a KernelMessage containing comm info response for all comms when target name is empty string") {
+    val kernelMessage = new KernelMessage(
+      Seq[String](), "test message", header, header, Map[String, String](), "{\"target_name\":\"\"}"
+    )
+
+    when(mockCommStorage.getTargets()).thenReturn(Set("test.name1", "test.name2"))
+    when(mockCommStorage.getCommIdsFromTarget("test.name1")).thenReturn(Option(scala.collection.immutable.IndexedSeq("1", "2")))
+    when(mockCommStorage.getCommIdsFromTarget("test.name2")).thenReturn(Option(scala.collection.immutable.IndexedSeq("3", "4")))
+
+    actor ! kernelMessage
+    val reply = relayProbe.receiveOne(1.seconds).asInstanceOf[KernelMessage]
+    val commInfo = Json.parse(reply.contentString).as[CommInfoReply]
+
+    commInfo.comms.size should equal (4)
+    commInfo.comms.get("1").get.get("target_name").get should be ("test.name1")
+    commInfo.comms.get("2").get.get("target_name").get should be ("test.name1")
+    commInfo.comms.get("3").get.get("target_name").get should be ("test.name2")
+    commInfo.comms.get("4").get.get("target_name").get should be ("test.name2")
+  }
+
+  it("should return a KernelMessage containing an empty comm info response when target name is not found") {
+    val kernelMessage = new KernelMessage(
+      Seq[String](), "test message", header, header, Map[String, String](), "{\"target_name\":\"can't_find_me\"}"
+    )
+
+    when(mockCommStorage.getTargets()).thenReturn(Set("test.name"))
+    when(mockCommStorage.getCommIdsFromTarget("test.name")).thenReturn(Option(scala.collection.immutable.IndexedSeq("1", "2")))
+
+    actor ! kernelMessage
+    val reply = relayProbe.receiveOne(1.seconds).asInstanceOf[KernelMessage]
+    val commInfo = Json.parse(reply.contentString).as[CommInfoReply]
+
+    commInfo.comms.size should equal (0)
+  }
+}
