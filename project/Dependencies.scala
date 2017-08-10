f9d64744df3388bfd3418c1c1724ff8dffc42893
Luciano Resende <lresende@apache.org>
2017-08-10 12:47:26 -0700

    [TOREE-432] Update Akka to version 2.4.20
    Address akka vulnerability: CVE-2017-5643
    
    Closes #132

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,9 +22,9 @@ object Dependencies {
 
   // Libraries
 
-  val akkaActor = "com.typesafe.akka" %% "akka-actor" % "2.4.17" // Apache v2
-  val akkaSlf4j = "com.typesafe.akka" %% "akka-slf4j" % "2.4.17" // Apache v2
-  val akkaTestkit = "com.typesafe.akka" %% "akka-testkit" % "2.4.17" // Apache v2
+  val akkaActor = "com.typesafe.akka" %% "akka-actor" % "2.4.20" // Apache v2
+  val akkaSlf4j = "com.typesafe.akka" %% "akka-slf4j" % "2.4.20" // Apache v2
+  val akkaTestkit = "com.typesafe.akka" %% "akka-testkit" % "2.4.20" // Apache v2
 
   val clapper = "org.clapper" %% "classutil" % "1.0.12" // BSD 3-clause license, used for detecting plugins
 
