cde9d5f921b79a6a969572b90595beee34818233
Cheng Pan <chengpan@apache.org>
2023-08-10 18:05:24 +0800

    [TOREE-546] Fix ZeroMQSocketRunnableSpec hang

--- communication/src/test/scala/org/apache/toree/communication/socket/ZeroMQSocketRunnableSpec.scala
+++ communication/src/test/scala/org/apache/toree/communication/socket/ZeroMQSocketRunnableSpec.scala
@@ -58,6 +58,7 @@ class ZeroMQSocketRunnableSpec extends FunSpec with Matchers
     mockSocketType = SocketType.RAW // mock[SocketType]
     zmqContext = ZMQ.context(1)
     pubSocket = zmqContext.socket(SocketType.PUB)
+    pubSocket.setLinger(10 * 1000)
   }
 
   after {
